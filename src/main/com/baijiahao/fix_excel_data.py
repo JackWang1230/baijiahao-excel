f6567bf53e11d107d2c66e9dbed5cb6e48ac4246
wangrui2 <wangrui2@cstor.cn>
2023-05-11 15:26:19 +0800

    inital commit

---
+++ src/main/com/baijiahao/fix_excel_data.py
@@ -0,0 +1,63 @@
+import openpyxl
+import datetime
+from natsort import natsorted
+
+
+def read_excel():
+    workbook = openpyxl.load_workbook('/Users/wangrui/Documents/coding/python/baojiahao/resources/123.xlsx')
+    # workbook = openpyxl.load_workbook(
+    #     '/Users/wangrui/Documents/coding/python/baojiahao/resources/kanye-say-history.xlsx')
+    worksheet = workbook['Sheet']
+    n_rows = worksheet.max_row
+    start = 1
+    index = 0
+    for i in worksheet.iter_rows(min_row=2, values_only=True):
+        index += 1
+        print(index)
+        total_read = i[1]
+        total_comment = i[3]
+        if '万' and '.' in total_read:
+            fix_read_value = int(total_read.replace('.', '').replace('万', '') + '000')
+            worksheet.cell(index + 1, 2, fix_read_value)
+        elif '万' in total_read:
+            fix_read_value = int(total_read.replace('万', '') + '0000')
+            worksheet.cell(index + 1, 4, fix_read_value)
+        else:
+            fix_read_value = int(total_read)
+            worksheet.cell(index + 1, 2, fix_read_value)
+
+        if '万' and '.' in total_comment:
+            fix_comment_value = int(total_comment.replace('.', '').replace('万', '') + '000')
+            worksheet.cell(index + 1, 4, fix_comment_value)
+        elif '万' in total_comment:
+            fix_comment_value = int(total_comment.replace('万', '') + '0000')
+            worksheet.cell(index + 1, 4, fix_comment_value)
+        else:
+            fix_comment_value = int(total_comment)
+            worksheet.cell(index + 1, 4, fix_comment_value)
+
+        process = (index - start) / (n_rows - start) * 100
+        print("处理进度:", process, "%")
+    print("开始保存:", datetime.datetime.now())
+    workbook.save('kanye-fix-total_read.xlsx')
+    print("完成保存:", datetime.datetime.now())
+
+
+def sort_by_total_read():
+    workbook = openpyxl.load_workbook('kanye-fix-total_read.xlsx')
+    worksheet = workbook['Sheet']
+    sorted_rows = natsorted(worksheet.rows, key=lambda x: x[1].value, reverse=True)
+    wb_new = openpyxl.Workbook()
+    sheet_new = wb_new.active
+    for row in sorted_rows:
+        sheet_new.append([cell.value for cell in row])
+
+    print("开始保存:", datetime.datetime.now())
+    wb_new.save('kanye-fixed-total_read.xlsx')
+    print("完成保存:", datetime.datetime.now())
+
+
+if __name__ == '__main__':
+
+    read_excel()
+    sort_by_total_read()
---
+++ src/main/com/baijiahao/read_fixed_excel_data.py
@@ -0,0 +1,53 @@
+import openpyxl
+import os
+import requests
+from datetime import datetime, timedelta
+
+
+def read_excel():
+    workbook = openpyxl.load_workbook('kanye-fixed-total_read.xlsx')
+    worksheet = workbook['Sheet']
+    worksheet.cell(row=1, column=7, value='图片下载路径')
+    n_rows = worksheet.max_row
+    start = 1
+    index = 0
+    day_index = 0
+    today = datetime.today()
+    for i in worksheet.iter_rows(min_row=2, values_only=True):
+
+        if index % 15 == 0:
+            folder_time = today + timedelta(days=day_index)
+            day_index += 1
+            true_day = folder_time.strftime('%Y-%m-%d')
+            fold_name = f'{true_day}-start-{index + 2}'
+            os.makedirs(fold_name, exist_ok=True)
+        index += 1
+        url_list = []
+        image_url = i[6]
+        image_url_list = image_url.split(",https")
+        if len(image_url_list) > 1:
+            url_list.append(image_url_list[0])
+            for v in range(1, len(image_url_list)):
+                url_list.append('https' + image_url_list[v])
+        else:
+            url_list.append(image_url_list[0])
+
+        image_filename = ''
+        for j in range(len(url_list)):
+            response = requests.get(url_list[j])
+            image_filename += str(index + 1) + "-" + str(j) + ".jpeg"
+            with open(os.path.join(fold_name, image_filename), 'wb') as f:
+                f.write(response.content)
+
+        worksheet.cell(index + 1, 7, image_filename)
+        process = (index - start) / (n_rows - start) * 100
+
+        print("处理进度:", process, "%")
+
+    print("开始保存:", datetime.now())
+    workbook.save('kanye_finished.xlsx')
+    print("完成保存:", datetime.now())
+
+
+if __name__ == '__main__':
+    read_excel()
